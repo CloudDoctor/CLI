61339790c76a552a205ef18fe8f4d97d4e50375b
Matthew Baggett <matthew@baggett.me>
2018-09-30 13:47:50 +0100

    Fix docker cluster join bug. Fix hostname bug. Fix hostname-to-docker propogation bug.

--- assets/hostname.sh
+++ assets/hostname.sh
@@ -3,3 +3,6 @@ hostname -F /etc/hostname
 cp /etc/bash.bashrc /etc/bash.bashrc.backup
 sed -i 's/\\h/\\e\[31m\\H\\e\[0m/' /etc/bash.bashrc
 sed -i 's/\\u/\\e\[5m\\u\\e\[0m/' /etc/bash.bashrc
+
+# Incase Docker is using the old hostname, lets restart it:
+/etc/init.d/docker restart
